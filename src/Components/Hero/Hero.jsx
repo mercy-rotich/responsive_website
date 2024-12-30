c1c10b7e74379b6f182909b49a903d9d3c3e494c
mercy-rotich <cherotichm182@gmail.com>
2024-12-30 00:58:22 -0800

    designed the hero section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import'./Hero.css'
+import dark_arrow from '../../assets'
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+<div className="hero-text">
+    <h1>We ensure better education for a better world</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda recusandae tempora vitae beatae labore quidem sequi sapiente velit. Commodi natus ex sapiente nam.</p>
+    <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
+</div>
+    </div>
+  )
+}
+
+export default Hero
